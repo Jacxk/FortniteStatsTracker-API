57322d6b0bfdb14b6a276088908e44425c679f30
Jack <tfermin1827@hotmail.com>
2018-05-09 12:12:03 -0400

    Added a responce for the root page, and added a status reponse

--- index.js
+++ index.js
@@ -11,74 +11,83 @@ const cooldown = new Cooldown(config.cooldown);
 const queue = new Queue(config.globalCooldown);
 const fortnite = new FortniteClient(process.env.API_TOKEN);
 
+app.get('/', (req, res) => {
+    res.status(404).send({
+        success: false,
+        code: 404,
+        time: performance.now() - start,
+        data: 'Page Not Found'
+    });
+});
+
 app.get('/v1/u=:username&p=:platform', (req, res) => {
 
-  const start = performance.now();
+    const start = performance.now();
 
-  let ip;
-  if (req.headers['x-forwarded-for']) ip = req.headers['x-forwarded-for'].split(',').pop();
-  else ip = req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-  if (!ip) return res.send({
-    success: false,
-    code: 500,
-    time: performance.now() - start,
-    data: 'Internal Server Error'
-  });
+    let ip;
+    if (req.headers['x-forwarded-for']) ip = req.headers['x-forwarded-for'].split(',').pop();
+    else ip = req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+    if (!ip) return res.status(500).send({
+        success: false,
+        code: 500,
+        time: performance.now() - start,
+        data: 'Internal Server Error'
+    });
 
-  if (cooldown.active(ip)) return res.send({
-    success: false,
-    code: 420,
-    time: performance.now() - start,
-    data: 'Access denied, requesting too often'
-  });
+    if (cooldown.active(ip)) return res.status(420).send({
+        success: false,
+        code: 420,
+        time: performance.now() - start,
+        data: 'Access denied, requesting too often'
+    });
 
-  if ((queue.queue.length + 1) * config.globalCooldown > 30000) return res.send({
-    success: false,
-    code: 429,
-    time: performance.now() - start,
-    data: 'Requests are limited, please try again in a few seconds...'
-  });
+    if ((queue.queue.length + 1) * config.globalCooldown > 30000) return res.status(429).send({
+        success: false,
+        code: 429,
+        time: performance.now() - start,
+        data: 'Requests are limited, please try again in a few seconds...'
+    });
 
-  const inQueue = queue.add({
-    req,
-    res,
-    ip,
-    start
-  });
+    const inQueue = queue.add({
+        req,
+        res,
+        ip,
+        start
+    });
 
-  if (inQueue) res.end();
+    if (inQueue) res.end();
 });
 
 queue.on('cycle', request => {
 
-  fortnite.user(request.req.params.username, request.req.params.platform).then(stats => {
+    fortnite.user(request.req.params.username, request.req.params.platform).then(stats => {
 
-    console.log('Success!');
-    cooldown.add(request.ip);
+        console.log('Success!');
+        cooldown.add(request.ip);
 
-    return request.res.send({
-      success: true,
-      code: 200,
-      time: performance.now() - request.start,
-      data: stats
+        return request.res.status(200).send({
+            success: true,
+            code: 200,
+            time: performance.now() - request.start,
+            data: stats
+        });
+
+    }).catch(e => {
+
+        console.log('Failure!');
+        cooldown.add(request.ip);
+
+        return request.res.status(500).send({
+            success: false,
+            code: 500,
+            time: performance.now() - request.start,
+            data: 'Internal Server Error...'
+        });
+
     });
-
-  }).catch(e => {
-
-    console.log('Failure!');
-    cooldown.add(request.ip);
-
-    return request.res.send({
-      success: false,
-      code: 500,
-      time: performance.now() - request.start,
-      data: 'Internal Server Error...'
-    });
-
-  });
 
 });
 
 const server = app.listen(process.env.PORT || 8080, () => {
-  console.log(`Listening on port number ${server.address().port}`);
+    console.log(`Listening on port number ${server.address().port}`);
 });
